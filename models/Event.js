401c8fbdabb28a4bc3e6e5ab2b5ed39346a34cd4
Munaa8805 <munaa.tsetsegmaa@gmail.com>
2026-03-06 15:13:54 -0700

    add some folder

---
+++ models/Event.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+}, { timestamps: true });
+
+const Event = mongoose.model("Event", eventSchema);
+
+module.exports = Event;
